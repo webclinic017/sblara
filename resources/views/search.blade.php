3e0363b5f5832b78763cc1f4e8234dabb81ddcba
Farzoqe Khan <farzoqe@gmail.com>
2017-05-03 13:19:41 +0500

    search changes

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -5,6 +5,23 @@
     <a href="javascript:;" class="btn submit md-skip">
         <i class="fa fa-search"></i>
     </a>
+    <a href="javascript:;" class="btn md-skip ">
+        <i class="fa fa-gear show-search-options" style="right: 40px"></i>
+    </a>
+    <div class="search-result-options hidden">
+        <div class="form-group">
+            <div class="mt-checkbox-list">
+                <label class="mt-checkbox"> Exact match only
+                    <input type="checkbox" value="1" name="exact_match" />
+                    <span></span>
+                </label>
+                <label class="mt-checkbox"> Search in title (code)
+                    <input type="checkbox" value="1" name="title_search" />
+                    <span></span>
+                </label>
+            </div>
+        </div>
+    </div>
     <div class="search-result-ajax-top hidden">
         <div class="search-result-ajax">
             <div class="asp_group_header"></div>
