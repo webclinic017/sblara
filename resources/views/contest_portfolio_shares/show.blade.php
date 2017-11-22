b1ff9441241f1d33775106bd2e1de121add97e33
AK Azad <akazadcse@gmail.com>
2017-11-22 17:42:26 +0600

    contests portfolio

--- resources/views/contest_portfolio_shares/show.blade.php
+++ resources/views/contest_portfolio_shares/show.blade.php
@@ -11,24 +11,29 @@
                         <a href="">
                             <span class="text-primary"></span>
                         </a>
-                         Details
-                    </span>
+                         Portfolio
+                    </span><br><br>
+                    <a href="{{ route('portfolios.shares.create', $portfolio) }}">Buy Share</a>
                 </div>
             </div>
 
             <div class="portlet-body">
-                <table class="table table-bordered">
+                <table class="table table-bordered table-condensed">
                     <thead>
-                        <tr>
-                            <th colspan="6">
+                        <tr class="bg-primary">
+                            <th colspan="2"></th>
+                            <th class="text-center" colspan="3">
                                 <span class="caption-subject bold uppercase">
                                     <span class="text-primary"></span>
-                                    Your Position is () Among () Participants 
+                                    Today
                                 </span>
                             </th>
+                            <th colspan="3"></th>
+                            <th class="text-center" colspan="4">Since Purchased</th>
+                            <th colspan="2"></th>
                         </tr>
 
-                        <tr class="text-center">
+                        <tr class="text-center active">
                             <th class="text-center">Company Code</th>
                             <th class="text-center">Last Trade Price</th>
                             <th class="text-center">Change</th>
@@ -42,94 +47,183 @@
                             <th class="text-center">% Change</th>
                             <th class="text-center">% Portfolio</th>
                             <th class="text-center">Sell Value</th>
+                            <th class="text-center">Sell</th>
                         </tr>
                     </thead>
 
-                    <tbody>
+                    <tbody> 
+                        @php
+                            $sumGainLoss = 0;
+                            $sumTotalPurchase = 0;
+                            $sumTotalGain = 0;
+                            $sumSellValueDeductingCommision = 0;
+                        @endphp
+
                         @forelse ($portfolio->shares as $share)
-                            @if (count($portfolio->shares) > 1)
-                                @if ($loop->first)
-                                    <tr>
-                                        <td>
-                                            {{ $share->intrument->instrument_code }}
-                                            <small class="instrument-name">{{ $share->intrument->name }}</small>
-                                        </td>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                        <td>{{ $share->sum('no_of_shares') }}</td>
-                                        <td>{{ $share->buying_price }}</td>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                    </tr>
-                                @endif
-                            @endif
+                            @php
+                                $instrumentCode = $share->intrument->instrument_code;
+                                $instrumentName = $share->intrument->name;
+                                $portfolioCashAmount = $portfolio->cash_amount;
+                                $noOfShare = $share->no_of_shares;
+                                $sumNoOfShare = $portfolio->shares->sum('no_of_shares');
+                                
+                                $buyingPrice = $share->buying_price;
+                                $totalBuyCost  = $noOfShare * $buyingPrice;
+
+                                $lastTradePrice = number_format($share->intrument->data_banks_intraday->close_price, 2);
+                                $lastTradeDate = $share->intrument->data_banks_intraday->lm_date_time->format('Y-m-d');
+                                $priceChange = number_format($share->intrument->data_banks_intraday->price_change, 2);
+                                $gainLoss = $priceChange * $noOfShare;
+                                $sumGainLoss += $gainLoss;
+
+                                $shareComission = $share->commission;
+                                $buyCommission = $shareComission * $totalBuyCost / 100;
+                                
+                                $totalPurchase = $buyingPrice * $noOfShare + $buyCommission;
+                                $sumTotalPurchase += $totalPurchase;
+
+                                $sellValue = $noOfShare * $lastTradePrice;
+                                $sellCommission = ($shareComission / 100) * $sellValue;
+                                $sellValueDeductingCommision = $sellValue - $sellCommission;
+                                $sumSellValueDeductingCommision += $sellValueDeductingCommision;
+
+                                $totalBuyCostWithCommission = $totalBuyCost + $buyCommission;
+                                $totalGain = $sellValueDeductingCommision - $totalBuyCostWithCommission;
+                                $sumTotalGain += $totalGain;
+
+                                $percentChange = number_format($totalGain / $totalBuyCostWithCommission * 100, 2);
+
+                                $allShareCashAmount = $sumNoOfShare * $buyingPrice;
+                                $totalPortfolioValue = $allShareCashAmount + $portfolioCashAmount;
+                                $percentPortfolio = number_format($sellValue / $totalPortfolioValue * 100, 2);
+                                $portfolioOfCash = number_format($portfolioCashAmount / $totalPortfolioValue * 100, 2);
+                                
+                                /* New */
+                                $sumBuyCommissionLoop = $sellValue;
+                                $sumTotalPurchaseLoop = $sellValue;
+                                $sumTotalGainLoop = $sellValue;
+                                $growthPercent = $sellValue;
+                                
+                                $sumPercentPortfolioLoop = $percentPortfolio;
+                            @endphp
 
                             <tr>
                                 <td>
-                                    {{ $share->intrument->instrument_code }}
-                                    <small class="instrument-name">{{ $share->intrument->name }}</small>
+                                    <span class="bold text-primary">{{ $instrumentCode }}</span>
+                                    <br>
+                                    <small class="instrument-name">{{ $instrumentName }}</small>
                                 </td>
                                 <td>
-                                    {{ $lastTradePrice }}
-                                    <small class="instrument-name">({{ $lastTradeDate }})</small>
+                                    {{ $lastTradePrice }} <br>
+                                     <small class="instrument-name">
+                                        ({{ $lastTradeDate }})
+                                    </small>
                                 </td>
-                                <td>{{ $change }}</td>
-                                <td>{{ $gainLossToday }}</td>
-                                <td>{{ $share->no_of_shares }}</td>
-                                <td>{{ $share->buying_price }}</td>
                                 <td>
-                                    {{ $share->buying_date->format('Y-m-d') }}
+                                    {{ $priceChange }}
                                 </td>
-                                <td>{{ $commission }}</td>
+                                <td>
+                                    @if ($gainLoss > 0)
+                                        <span class="text-success">{{ $gainLoss }}</span>
+                                    @else
+                                        <span class="text-danger">{{ $gainLoss }}</span>
+                                    @endif
+                                </td>
+                                <td>{{ $noOfShare }}</td>
+                                <td>{{ $buyingPrice }}</td>
+                                <td>{{ $share->buying_date->format('Y-m-d')  }}</td>
+                                <td>{{ $buyCommission }}</td>
                                 <td>{{ $totalPurchase }}</td>
-                                <td>{{ $gainLossTotal }}</td>
-                                <td>{{ $percentChange }}</td>
-                                <td>{{ $percentPortfolio }}</td>
-                                <td>{{ $sellValue }}</td>
+                                <td>
+                                    @if ($totalGain > 0)
+                                        <span class="text-success">{{ $totalGain }}</span>
+                                    @else
+                                        <span class="text-danger">{{ $totalGain }}</span>
+                                    @endif
+                                </td>
+                                <td>
+                                    @if ($percentChange > 0)
+                                        <span class="text-success">{{ $percentChange }}%</span>
+                                    @else
+                                        <span class="text-danger">{{ $percentChange }}%</span>
+                                    @endif
+                                </td>
+                                <td>
+                                    {{ $percentPortfolio }}%
+                                </td>
+                                <td>{{ $sellValueDeductingCommision }}</td>
+                                <td>
+                                    @if ($share->is_mature)
+                                        <small>Matured</small>
+                                    @else
+                                        <small>Not Matured</small>
+                                    @endif
+                                </td>
                             </tr>
 
                             @if ($loop->last)
-                                <tr>
-                                    <td>Cash</td>
+                                <tr class="active">
+                                    <td colspan="3">
+                                        <span class="bold">Cash</span>
+                                    </td>
+                                    <td></td>
+                                    <td colspan="3"></td>
                                     <td></td>
                                     <td></td>
                                     <td></td>
                                     <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
+                                    <td>{{ $portfolioOfCash }}%</td>
+                                    <td>
+                                        <span class="bold">
+                                            {{ number_format($portfolioCashAmount, 2) }}
+                                        </span>
+                                    </td>
                                     <td></td>
                                 </tr>
 
-                                <tr>
-                                    <td>Total</td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
+                                <tr class="active">
+                                    <td colspan="3">
+                                        <span class="bold">Total</span>
+                                    </td>
+                                    <td>
+                                        @if ($sumGainLoss > 0)
+                                            <span class="text-success">{{ number_format($sumGainLoss, 2) }}</span>
+                                        @else
+                                            <span class="text-danger">{{ number_format($sumGainLoss, 2) }}</span>
+                                        @endif
+                                    </td>
+                                    <td>{{ $sumNoOfShare }}</td>
+                                    <td colspan="2"></td>
+                                    <td>{{ $sumBuyCommissionLoop }}</td>
+                                    <td>{{ $sumTotalPurchaseLoop }}</td>
+                                    <td>
+                                        @if ($sumTotalGainLoop > 0)
+                                            <span class="text-success">{{ $sumTotalGainLoop }}</span>
+                                        @else
+                                            <span class="text-danger">{{ $sumTotalGainLoop }}</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @if ($growthPercent > 0)
+                                            <span class="text-success">{{ $growthPercent }}%</span>
+                                        @else
+                                            <span class="text-danger">{{ $growthPercent }}%</span>
+                                        @endif
+                                    </td>
+                                    <td>{{ $portfolioOfCash += $sumPercentPortfolioLoop }}%</td>
+                                    <td>
+                                        <span class="bold">
+                                            {{ number_format($sumSellValueDeductingCommision += $portfolioCashAmount, 2) }}
+                                        </span>
+                                    </td>
                                     <td></td>
                                 </tr>
                             @endif
                         @empty     
                             <tr class="no-records-found text-center">
-                                <td colspan="13">No portfolio available. Please <a href="{{ route('portfolios.shares.create', $portfolio) }}">buy share</a> to create your portfolio.</td>
+                                <td colspan="13">
+                                    No portfolio available. Please <a href="{{ route('portfolios.shares.create', $portfolio) }}">buy share</a> to create your portfolio.
+                                </td>
                             </tr>
                         @endforelse
                     </tbody>
